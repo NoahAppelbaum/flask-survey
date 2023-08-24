276c9b0193a5e88cc51a3902d1177a45b5750180
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-24 14:20:41 -0500

    refactored answer handling to exercise specs and finished Thank You page

--- app.py
+++ app.py
@@ -13,14 +13,39 @@ responses = []
 
 @app.get('/')
 def start_survey():
+    """Displays survey start page"""
     # need title and instructions
     return render_template("survey_start.html",
                            title=survey.title,
                            instructions=survey.instructions)
 
 
-@app.post("/questions/<question_number>")
+@app.get("/questions/<question_number>")
 def show_question(question_number):
+    """Displays current survey question"""
+
     return render_template("question.html",
                            question_number=int(question_number),
                            question=survey.questions[int(question_number)])
+
+
+@app.post("/answer/<question_number>")
+def handle_answer(question_number):
+    """Processes question answer and redirects to next question"""
+
+    responses.append(request.form[f"{question_number}"])
+    print("responses", responses)
+
+    if int(question_number) + 1 >= len(survey.questions):
+        return redirect("/thank-you")
+
+    return redirect(f"/questions/{int(question_number) + 1}")
+
+
+@app.get("/thank-you")
+def show_thankyou():
+    """Shows thank you page"""
+
+    return render_template("completion.html",
+                           responses=responses,
+                           questions=survey.questions)
