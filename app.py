905735b5e045b9bbbda928713a50acdc0e0869c3
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-24 17:30:04 -0500

    small changes in accordance with code review

--- app.py
+++ app.py
@@ -20,26 +20,26 @@ def start_survey():
                            instructions=survey.instructions)
 
 
-@app.get("/questions/<question_number>")
+@app.get("/questions/<int:question_number>")
 def show_question(question_number):
     """Displays current survey question"""
 
     return render_template("question.html",
-                           question_number=int(question_number),
-                           question=survey.questions[int(question_number)])
+                           question_number=question_number,
+                           question=survey.questions[question_number])
 
 
-@app.post("/answer/<question_number>")
+@app.post("/answer/<int:question_number>")
 def handle_answer(question_number):
     """Processes question answer and redirects to next question"""
 
-    responses.append(request.form[f"{question_number}"])
+    responses.append(request.form["answer"])
     print("responses", responses)
 
-    if int(question_number) + 1 >= len(survey.questions):
+    if question_number + 1 >= len(survey.questions):
         return redirect("/thank-you")
 
-    return redirect(f"/questions/{int(question_number) + 1}")
+    return redirect(f"/questions/{question_number + 1}")
 
 
 @app.get("/thank-you")
