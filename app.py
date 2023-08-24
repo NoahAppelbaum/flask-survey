27691d2f169dcd0dd81b7ef62849125cdaa35032
Elise Ngo <elisetvy@gmail.com>
2023-08-24 11:45:07 -0700

    Create start & question page

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get('/')
+def start_survey():
+    # need title and instructions
+    return render_template("survey_start.html",
+                           title=survey.title,
+                           instructions=survey.instructions)
+
+
+@app.post("/questions/<question_number>")
+def show_question(question_number):
+    return render_template("question.html",
+                           question_number=int(question_number),
+                           question=survey.questions[int(question_number)])
